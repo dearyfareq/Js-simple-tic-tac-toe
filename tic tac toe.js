5673d4c061e389a2e23f775b8ae28ce0888c544c
Diyari <dearyfareq15@gmail.com>
2023-01-27 18:54:57 +0300

    trying to add winning condition

--- tic tac toe.js
+++ tic tac toe.js
@@ -6,21 +6,22 @@ let condition2 = 1;
 
 
 const winningCondition = [
-    [cells[1] == cells[2] == cells[3]],
-    [cells[4] == cells[5] == cells[6]],
-    [cells[7] == cells[8] == cells[9]],
-    [cells[1] == cells[4] == cells[7]],
-    [cells[2] == cells[5] == cells[8]],
-    [cells[3] == cells[6] == cells[9]],
-    [cells[1] == cells[5] == cells[9]],
-    [cells[3] == cells[5] == cells[7]],
+    [cells[1] == cells[2] == cells[3] ],
+    [cells[7] == cells[8] == cells[9] ],
+    [cells[1] == cells[4] == cells[7] ],
+    [cells[2] == cells[5] == cells[8] ],
+    [cells[3] == cells[6] == cells[9] ],
+    [cells[1] == cells[5] == cells[9] ],
+    [cells[3] == cells[5] == cells[7] ],
 ]
+
+const allEqual = arr => arr.every( v => v === (1 || 2) )
 
 cells.forEach(index => 
 {
     index.value = range;
 })
-//console.table(cells);
+
 
 function handleUpdate(e){
 
@@ -29,29 +30,21 @@ function handleUpdate(e){
     this.style.background = "cyan";
 
     this.value = 2;
-
-    console.log(1)
      
     while( (condition <= 1 && condition2 < 5)){
     var num = Math.floor(Math.random() * cells.length);
 
-    console.log(2)
-
     if(cells[num].value == 0){
 
-        console.log(3)
-
-        cells[num].innerHTML = 'red'
+        cells[num].style.background = 'red'
 
         cells[num].value = 1
 
-        condition++
-        condition2++
+        condition++;
+        condition2++;
 
 
     }
-
-
     }
 
 }
